73bc866fa88d8c48d5cba9490ac27f4f36b09b7a
Devjoe <benagu477@gmail.com>
2024-10-07 20:46:28 +0100

    bug on url

--- src/utils/BaseURL.jsx
+++ src/utils/BaseURL.jsx
@@ -1,8 +1,8 @@
 
 
  export default {
-    URL:'http://localhost:3040/api/v1/user',
-    SOCKET_URL:'http://localhost:3040/chat',
+    URL:'http://https://lodgemerentalsbackend.onrender.com/api/v1/user',
+    SOCKET_URL:'https://lodgemerentalsbackend.onrender.com/chat',
    LOCAL_URL:'http://localhost:3040/api/v1/user',
    STAGING_URL:'https://lodgemerentalsbackend.onrender.com/api/v1/user',
    LOCAL_URL_SOCKET:'http://localhost:3040/chat',
