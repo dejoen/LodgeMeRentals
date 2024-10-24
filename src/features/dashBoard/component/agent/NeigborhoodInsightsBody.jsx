901e52c0e3cd013ad78f19fe96cd8c19c7444bc6
Devjoe <benagu477@gmail.com>
2024-10-24 15:14:10 +0100

    update

--- src/features/dashBoard/component/agent/NeigborhoodInsightsBody.jsx
+++ src/features/dashBoard/component/agent/NeigborhoodInsightsBody.jsx
@@ -1,10 +1,11 @@
+import { LinearProgress } from "@mui/material";
 import { PieChart } from "@mui/x-charts";
 
 const NeighborHoodInsights = () => {
   return (
-    <div className="md:ms-10 mt-10 w-full">
+    <div className="md:ms-2 mt-10 w-full font-nunito">
       <div className="w-full flex flex-wrap gap-5 place-content-center ">
-        <div className="relative max-w-fit border border-black rounded-lg  p-5">
+        <div className="relative max-w-fit border border-black rounded-lg  p-5 m-5">
           <p>Residence Database</p>
           <div className="absolute w-[250px] h-[200px] flex place-items-center justify-center ">
             <p>Area name</p>
@@ -26,34 +27,42 @@ const NeighborHoodInsights = () => {
         </div>
 
         <div className=" flex flex-wrap place-content-center gap-5">
-          <div className=" w-[300px] h-[300px] border border-black rounded-lg">
-            <p>
+          <div className=" w-[300px] max-h-fit p-5 border border-black rounded-lg">
+            <p className="font-bold mb-2">
               Area Compatability
             </p>
             <p>
-              Area Compatability
+              Number of bus stops:<span>0</span>
             </p>
             <p>
-              Area Compatability
+            Number of schools:<span>0</span>
             </p>
             <p>
-              Area Compatability
+            Number of industries:<span>0</span>
             </p>
           </div>
 
-           <div className=" w-[300px] h-[300px] border border-black rounded-lg">
-            <p>
-              Area Compatability
+           <div className=" w-[300px] max-h-fit p-5 border border-black rounded-lg">
+            <p className="font-bold mb-2">
+              
+              {"LandLord's  Insight"}
             </p>
-            <p>
-              Area Compatability
-            </p>
-            <p>
-              Area Compatability
-            </p>
-            <p>
-              Area Compatability
-            </p>
+             <div className="flex flex-wrap place-items-center    gap-5" >
+              <p className="">hh</p>
+              <LinearProgress value={30}  variant="determinate" sx={{width:200, height:10, borderRadius:3,}}  />
+             </div>
+             <div className="flex flex-wrap place-items-center    gap-5" >
+              <p className="">hh</p>
+              <LinearProgress value={30}  variant="determinate" sx={{width:200, height:10, borderRadius:3,}}  />
+             </div>
+             <div className="flex flex-wrap place-items-center    gap-5" >
+              <p className="">hh</p>
+              <LinearProgress value={30}  variant="determinate" sx={{width:200, height:10, borderRadius:3,}}  />
+             </div>
+             <div className="flex flex-wrap place-items-center    gap-5" >
+              <p className="">hh</p>
+              <LinearProgress value={30}  variant="determinate" sx={{width:200, height:10, borderRadius:3,}}  />
+             </div>
           </div>
         </div>
       </div>
