718d717ef5a7a0fccb438bc7dddce6326431dc8e
Devjoe <benagu477@gmail.com>
2024-11-01 07:40:22 +0100

    update on listed houses

--- src/features/dashBoard/component/agent/listedhouses/ListedHousesBody.jsx
+++ src/features/dashBoard/component/agent/listedhouses/ListedHousesBody.jsx
@@ -1,12 +1,15 @@
+
 import ListedHousesCard from "./ListedHousesCard";
 
 
 const ListedHousesBody = () => {
+
     return (
     
-<div className="md:w-[96%] min-h-[50vh]   w-full md:m-5 text-black">
-    <div className="w-full border border-black border-opacity-100 rounded-md overflow-x-auto ">
-       <div className="w-full p-1 flex gap-4 justify-evenly md:justify-end pe-[2%]">
+<div className="md:w-[96%] min-h-[58vh]   w-full md:m-5 text-black ">
+
+    <div className="w-full border max-h-[56vh] md:max-h-[53vh] border-black border-opacity-100 rounded-t-md overflow-x-auto  ">
+       <div className=" w-[810px]  md:w-full p-1 flex gap-4 justify-end md:justify-end pe-[2%] sticky top-0 z-50">
         <p className="md:hidden  text-center min-w-[120px]"> Address</p>
         <p className="text-center min-w-[120px]">Clicks</p>
         <p className=" text-center min-w-[120px]">Impressions</p>
@@ -14,10 +17,23 @@ const ListedHousesBody = () => {
         <p className="text-center min-w-[120px]">Inquires</p>
         <p className=" text-center min-w-[120px]">applications</p>
        </div>
-       <div className="w-[810px] md:w-full h-[1px] bg-black"></div>
+       <div className="w-[810px] md:w-full h-[1px] bg-black "></div>
+       
+       <ListedHousesCard />
        <ListedHousesCard/>
+       <ListedHousesCard />
+       <ListedHousesCard />
+       <ListedHousesCard />
+     <ListedHousesCard />
        <ListedHousesCard/>
+       <ListedHousesCard />
+       <ListedHousesCard />
+       <ListedHousesCard />
+
+
+      
     </div>
+    <div className="w-full flex p-1 justify-end "><div className="bg-[#2A1910] ps-3 pe-3  pt-1 pb-1 rounded-md text-white">Last 30 days <span className="text-sm">{"v"}</span></div></div>
     
 </div>
 
--- src/features/dashBoard/component/agent/listedhouses/ListedHousesCard.jsx
+++ src/features/dashBoard/component/agent/listedhouses/ListedHousesCard.jsx
@@ -1,15 +1,27 @@
-
+  
+import image from '../../../../../assets/aboutbgimage.svg'
  const ListedHousesCard = () => {
+
 
     return (
       
-       <div className="w-full p-1 flex gap-4 justify-evenly md:justify-end pe-[2%] text-black">
-        <p className="md:hidden  text-center min-w-[120px]"> Address</p>
+       <div className=" p-1 flex gap-4 justify-evenly md:justify-end pe-[2%] text-black border-b border-black min-h-[10vh] w-[810px] md:w-full place-items-center" >
+        <div className=" text-center md:min-w-[300px] flex justify-center gap-5"> 
+         <div className='hidden md:block  h-[10vh] w-1/2 '><img className='rounded-md  w-full h-full    ' src={image}/></div>
+         <div className="text-sm flex flex-col justify-center place-items-center">
+         <p>House name</p>
+         <p>short description</p>
+         </div>
+        </div>
         <p className=" text-center min-w-[120px] max-w-[120px] overflow-x-auto">1</p>
         <p className=" text-center min-w-[120px]">0</p>
         <p className=" text-center min-w-[120px]">0</p>
         <p className=" text-center min-w-[120px]">4</p>
-        <p className=" text-center min-w-[120px]">4</p>
+        <div className=" text-center min-w-[120px] ps-4 flex justify-center gap-2">
+        <p >4</p>
+        <p>open</p>
+        </div>
+        
        </div>
     
     )
