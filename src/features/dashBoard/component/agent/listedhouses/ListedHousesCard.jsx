0b62c731421b7457ef0b94890fcc8ff036c69e0f
Devjoe <benagu477@gmail.com>
2024-11-13 22:32:25 +0100

    slidder

--- src/features/dashBoard/component/agent/listedhouses/ListedHousesCard.jsx
+++ src/features/dashBoard/component/agent/listedhouses/ListedHousesCard.jsx
@@ -1,36 +1,106 @@
 import PropTypes from "prop-types";
 import image from "../../../../../assets/aboutbgimage.svg";
-import  moreButton from '../../../../../assets/tableMore.svg'
-const ListedHousesCard = ({houseName,shortDescription,houseImage}) => {
+import moreButton from "../../../../../assets/tableMore.svg";
+import { useRef, useState } from "react";
+
+const ListedHousesCard = ({ houseName, shortDescription, houseImage }) => {
+ 
+  const containerRef = useRef();
+   const [scrollable,setScrollable] = useState(false)
+
+ 
   return (
-    <div className=" p-1 flex gap-4 justify-evenly md:justify-end pe-[2%] text-black border-b border-black min-h-[10vh] w-[810px] md:w-full place-items-center">
-      <div className="hidden md:block  h-[10vh]  ps-5 min-w-[120px] max-w-[120px] ">
-     
-          <img className="rounded-md  w-full h-full    " src={(houseImage)?houseImage:image} />
+    <div
+      ref={containerRef}
+      className={`flex text-black border-b border-black min-h-[10vh] w-[810px] md:w-full place-items-center  ${(scrollable) ?'overflow-x-auto' : 'overflow-x-hidden'} `}
+    >
+      <div
+       
+        className={` flex  gap-4 justify-evenly md:justify-end pe-[2%] text-black  place-items-center `}
+      >
+        <div className="hidden md:block  h-[10vh]  ps-5 min-w-[120px] max-w-[120px] ">
+          <img
+            className="rounded-md  w-full h-full    "
+            src={houseImage ? houseImage : image}
+          />
         </div>
         <div className="text-sm flex flex-col justify-center place-items-center  text-center min-w-[120px] max-w-[120px]">
-          <p>{(houseName)? houseName:'house name'}</p>
-          <p className="text-ellipsis">{(shortDescription)? shortDescription:'short description'}</p>
+          <p>
+            {houseName ? houseName : "house name"}
+          </p>
+          <p className="text-ellipsis">
+            {shortDescription ? shortDescription : "short description"}
+          </p>
         </div>
-    
-      <p className=" text-center min-w-[120px] max-w-[120px] overflow-x-auto">
-        1
-      </p>
-      <p className=" text-center min-w-[120px]">0</p>
-      <p className=" text-center min-w-[120px]">0</p>
-      <p className=" text-center min-w-[120px]">4</p>
-      <div className=" text-center min-w-[120px] ps-4 flex justify-center gap-3">
-        <p>4</p>
-       <img src={moreButton} className=""  alt="more.."/>
+
+        <p className=" text-center min-w-[120px] max-w-[120px] overflow-x-auto">
+          1
+        </p>
+        <p className=" text-center min-w-[120px]">0</p>
+        <p className=" text-center min-w-[120px]">0</p>
+        <p className=" text-center min-w-[120px]">4</p>
+        <div className=" text-center min-w-[120px] ps-4 flex justify-center gap-3">
+          <p>4</p>
+          <img
+            src={moreButton}
+            className=""
+            alt="more.."
+            onClick={() => {
+              setScrollable(!scrollable)
+              containerRef.current.scrollBy({ left: 1000, behavior: "smooth" });
+              setTimeout(()=>{
+                setScrollable(false)
+              },1000)
+            }}
+          />
+        </div>
+      </div>
+
+      <div
+       
+        className={`flex w-full  gap-4 justify-evenly md:justify-end pe-[2%] text-black  place-items-center `}
+      >
+      
+
+        <p className=" text-center min-w-[120px] max-w-[120px] overflow-x-auto">
+        Edit Listing
+        </p>
+        <p className=" text-center min-w-[120px]">
+          Delete
+        </p>
+        <p className=" text-center min-w-[120px]">Duplicate</p>
+        <p className=" text-center min-w-[120px]">Share</p>
+        <p className=" text-center min-w-[120px]">Pause</p>
+        <p className=" text-center min-w-[120px]">Mark as rented</p>
+        <div className=" text-center min-w-[120px] ps-4 flex justify-center gap-3">
+       
+          <img
+            src={moreButton}
+            className=""
+            alt="more.."
+            onClick={() => {
+              setScrollable(!scrollable)
+              containerRef.current.scrollBy({
+              
+                left:-1000,
+                behaviour: "smooth"
+              });
+
+              setTimeout(()=>{
+                setScrollable(false)
+              },1000)
+            }}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
- ListedHousesCard.propTypes = {
-   houseName:PropTypes.string,
-   shortDescription:PropTypes.string,
-   houseImage:PropTypes.string
- }
+ListedHousesCard.propTypes = {
+  houseName: PropTypes.string,
+  shortDescription: PropTypes.string,
+  houseImage: PropTypes.string
+};
 
 export default ListedHousesCard;
--- src/features/landingPage/component/NavbarComponent.jsx
+++ src/features/landingPage/component/NavbarComponent.jsx
@@ -1,193 +1,229 @@
 /* eslint-disable react/prop-types */
-import  LodgeMeIcon from '../../../assets/lodgeMeIcon.svg'
-import   sideImage  from '../../../assets/bgImage.svg'
-import   searchIcon  from '../../../assets/searchIcon.svg'
-import   menuIcon from '../../../assets/menuIcon.svg'
-import { openDrawer } from './NavDrawerBar'
-import { useNavigate } from 'react-router-dom'
-import { openGetStartedPageScreen } from '../screen/GetStartedScreen'
+import LodgeMeIcon from "../../../assets/lodgeMeIcon.svg";
+import sideImage from "../../../assets/bgImage.svg";
+import searchIcon from "../../../assets/searchIcon.svg";
+import menuIcon from "../../../assets/menuIcon.svg";
+import { openDrawer } from "./NavDrawerBar";
+import { useNavigate } from "react-router-dom";
+import { openGetStartedPageScreen } from "../screen/GetStartedScreen";
 
+const NavBarComponent = props => {
+  const navigate = useNavigate();
+  return (
+    <div className="nav-container">
+      <div className="nav-container-small-screen md:hidden">
+        <div className="w-full h-[50px] top-[21px] p-[10px] gap-[10px]  bg-white">
+          <div className=" flex  w-full ">
+            <img className="w-[41px] h-[30px]" src={LodgeMeIcon} />
+            <div className="flex w-full justify-end">
+              <div className="  w-[100px]  flex justify-between">
+                <img src={searchIcon} onClick={() => {}} />
 
-const NavBarComponent = (props) => {
-  const navigate = useNavigate()
-     return (
-        <div className='nav-container'>
+                <img
+                  src={menuIcon}
+                  onClick={() => {
+                    openDrawer();
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <div className='nav-container-small-screen md:hidden'>
+        <div
+          className={`hero-text-bg ${props.image}   bg-no-repeat bg-cover bg-center   w-full h-[461px] flex place-items-center bg-slate-500 `}
+        >
+          <div
+            className={`hero-text-container  w-full h-[355px]  top-[140px]  left-[10px] mx-auto bg-black bg-opacity-25  ${props.adjustHeight}`}
+          >
+            <div
+              className={`hero-text h-[219px] ${props.adjustHeight}  flex flex-col p-2`}
+            >
+              <div
+                className={`text-white h-[54px]  mt-[30px] size-[50px] w-[300px] ms-[20px] text-3xl font-nunito font-bold   leading-[50px] text-center place-self-center ${props.lineHeight} ${props.textSize}`}
+              >
+                {props.text}
+              </div>
 
-      <div className='w-full h-[50px] top-[21px] p-[10px] gap-[10px]  bg-white'>
+              <div
+                className={`mt-[126px] text-white font-bold text-center z-10 text-balance text-xl `}
+              >
+                Carefully selected, best homes thats meet with your rental needs
+              </div>
 
-  <div className=' flex  w-full '>
-    <img className='w-[41px] h-[30px]' src={LodgeMeIcon}/>
-    <div className="flex w-full justify-end">
-    <div className='  w-[100px]  flex justify-between'>
-      <img src={searchIcon} onClick={()=>{
-      
-        
-         
-      }}/>
-      
-      <img src={menuIcon} onClick={()=>{
-        openDrawer()}}/>
-    </div>
-    </div>
-   
-  </div>
+              <div
+                className="bg-yellow-600 rounded-2xl  mt-8 place-self-center p-4 text-white z-10 hover:bg-white hover:text-yellow-600"
+                onClick={() => {
+                  if (props.buttonOneText === "Get Started") {
+                    openGetStartedPageScreen();
+                  } else {
+                    navigate("/login");
+                  }
+                }}
+              >
+                <p>
+                  {props.buttonOneText}
+                </p>
+              </div>
+
+              <div
+                className={`${props.makeVisible} bg-white rounded-2xl  mt-5 place-self-center p-4 text-black z-10 hover:bg-white hover:text-yellow-600`}
+                onClick={() => {
+                  openGetStartedPageScreen();
+                }}
+              >
+                <p>Register</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-     <div className={`hero-text-bg ${props.image}   bg-no-repeat bg-cover bg-center   w-full h-[461px] flex place-items-center bg-slate-500 `}>
+      <div
+        className={`nav-container-large-screen  hidden md:block ${props.image}  bg-no-repeat bg-cover bg-center h-[600px]`}
+      >
+        <div
+          className={`bg-heroGradientImage  md:gap-1  w-full mx-auto h-24 top-10 p-2 gap-2 text-white `}
+        >
+          <div className="nav-container-body gap-10 flex  z-50 ">
+            <img src={LodgeMeIcon} className="" />
 
-     <div className={`hero-text-container  w-full h-[355px]  top-[140px]  left-[10px] mx-auto bg-black bg-opacity-25  ${props.adjustHeight}` }>
-
-        <div className={`hero-text h-[219px] ${props.adjustHeight}  flex flex-col p-2`}>
-
-           <div className={`text-white h-[54px]  mt-[30px] size-[50px] w-[300px] ms-[20px] text-3xl font-nunito font-bold   leading-[50px] text-center place-self-center ${props.lineHeight} ${props.textSize}`}>
-          {props.text}
-           </div>
-
-           <div className={`mt-[126px] text-white font-bold text-center z-10 text-balance text-xl `}>
-           Carefully selected, best homes thats meet with your rental needs   
-           </div>
-
-           <div className="bg-yellow-600 rounded-2xl  mt-8 place-self-center p-4 text-white z-10 hover:bg-white hover:text-yellow-600"  onClick={()=>{
-              if(props.buttonOneText === "Get Started"){
-                 openGetStartedPageScreen()
-                
-              }else{
-              
-                navigate('/login')
-              }
-           }}>
-             <p>{props.buttonOneText}</p>
-           </div>
-
-            <div className={`${props.makeVisible} bg-white rounded-2xl  mt-5 place-self-center p-4 text-black z-10 hover:bg-white hover:text-yellow-600`}  onClick={()=>{
-
-openGetStartedPageScreen()
-}}>
-            <p>Register</p>
-          </div>
-           
-
-        </div>
-
-     </div>
-   
-     </div>
-        </div>
-
-
-      <div className={`nav-container-large-screen  hidden md:block ${props.image}  bg-no-repeat bg-cover bg-center h-[600px]`}>
-
-       <div className={`bg-heroGradientImage  md:gap-1  w-full mx-auto h-24 top-10 p-2 gap-2 text-white `}>
-
-<div className="nav-container-body gap-10 flex  z-50 ">
-
-        <img src={LodgeMeIcon} className=""/>
-
-        <div className="w-full h-9 gap-5 md:gap-10  place-self-center  flex  place-items-center  md:block   md:h-fit xl:flex">
-          
-          <div className=" w-fit h-5 
+            <div className="w-full h-9 gap-5 md:gap-10  place-self-center  flex  place-items-center  md:block   md:h-fit xl:flex">
+              <div className=" w-fit h-5 
            size-4  flex  gap-x-0 md:gap-x-5  md:w-full md:place-content-evenly ">
+                <p
+                  className="hover:underline"
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  Home
+                </p>
+                <p
+                  className="hover:underline"
+                  onClick={() => {
+                    navigate("/about");
+                  }}
+                >
+                  About
+                </p>
+                <p
+                  className="hover:underline "
+                  onClick={() => {
+                    navigate("/blog/one");
+                  }}
+                >
+                  Blog
+                </p>
+                <p
+                  className="hover:underline "
+                  onClick={() => {
+                    navigate("/copyrights");
+                  }}
+                >
+                  Copyrights
+                </p>
+                <p
+                  className="hover:underline "
+                  onClick={() => {
+                    navigate("/terms");
+                  }}
+                >
+                  Terms & Conditions
+                </p>
+              </div>
 
-            <p className='hover:underline' onClick={()=>{navigate('/')
-             
-            }}>Home</p>
-            <p className='hover:underline' onClick={()=>{navigate('/about')}}>About</p>
-             <p className='hover:underline ' onClick={()=>{navigate('/blog/one')}} >Blog</p>
-             <p className='hover:underline '  onClick={()=>{navigate('/copyrights')}}>Copyrights</p>
-             <p className='hover:underline '  onClick={()=>{navigate('/terms')}}>Terms & Conditions</p>
-          </div>
-
-          <div className=" w-fit  h-10 gap-16
+              <div className=" w-fit  h-10 gap-16
            size-4 flex  ms-5  md:mt-5 md:w-full md:justify-center md:ms-0 ">
+                <div className="  flex w-fit rounded-md border-white border  md:w-1/2 ">
+                  <input
+                    type="text"
+                    className=" outline-none bg-none text-black w-[90%] "
+                    placeholder="hjkj w-1/2"
+                  />
 
+                  <p className="  bg-white text-black  w-[20%] h-full flex justify-end pe-1  place-items-center ">
+                    icon
+                  </p>
+                </div>
 
-             <div className="  flex w-fit rounded-md border-white border  md:w-1/2 "> 
-
-              <input type="text" className=" outline-none bg-none text-black w-[90%] " placeholder="hjkj w-1/2"  />
-
-              <p className= '  bg-white text-black  w-[20%] h-full flex justify-end pe-1  place-items-center '>icon</p>
-
-            
-               
-
-             </div>
-             
-             <div className="flex place-items-center bg-orange-500 rounded-md w-20 hover:bg-white hover:text-orange-500">
-               <p className="w-full text-center" onClick={()=>{
-                  navigate('/login')
-               }}> Log in</p>
+                <div className="flex place-items-center bg-orange-500 rounded-md w-20 hover:bg-white hover:text-orange-500">
+                  <p
+                    className="w-full text-center"
+                    onClick={() => {
+                      navigate("/login");
+                    }}
+                  >
+                    {" "}Log in
+                  </p>
                 </div>
                 <div className="flex place-items-center bg-white rounded-md w-20 text-black hover:bg-orange-500  hover:text-white">
-               <p className="w-full text-center" onClick={()=>{
-                openGetStartedPageScreen()
-               }}> Register</p>
+                  <p
+                    className="w-full text-center"
+                    onClick={() => {
+                      openGetStartedPageScreen();
+                    }}
+                  >
+                    {" "}Register
+                  </p>
                 </div>
+              </div>
+            </div>
           </div>
-
         </div>
-       
-        </div>
+        <div className="mx-auto w-full h-[600px]  mt-20">
+          <div className=" xl:flex  font-nunito ">
+            <div
+              className={`hero-text-container xl-w-[20%] h-[355px] ${props.adjustHeight}   top-[140px] left-[10px] mx-auto bg-black bg-opacity-25 `}
+            >
+              <div
+                className={`hero-text h-[219px] ${props.adjustHeight} flex flex-col `}
+              >
+                <div
+                  className={` text-white h-[54px]  mt-[10px] size-[50px] w-[300px] ms-[20px] text-3xl font-bold   leading-[50px] text-center place-self-center ${props.lineHeight} ${props.textSize}`}
+                >
+                  {props.text}
+                </div>
 
-        </div>
-   <div className="mx-auto w-full h-[600px]  mt-20">
-        <div className=" xl:flex  font-nunito ">
+                <div className="mt-[140px] text-white font-bold text-center z-10 text-balance text-xl">
+                  Carefully selected, best homes thats meet with your rental
+                  needs
+                </div>
 
-        <div className={`hero-text-container xl-w-[20%] h-[355px] ${props.adjustHeight}   top-[140px] left-[10px] mx-auto bg-black bg-opacity-25 `}>
+                <div
+                  className="bg-yellow-600 rounded-2xl  mt-12 place-self-center p-4 text-white z-10 hover:bg-white hover:text-yellow-600"
+                  onClick={() => {
+                    if (props.buttonOneText === "Get Started") {
+                      openGetStartedPageScreen();
+                    } else {
+                      navigate("/login");
+                    }
+                  }}
+                >
+                  <p>
+                    {props.buttonOneText}
+                  </p>
+                </div>
 
-        <div className={`hero-text h-[219px] ${props.adjustHeight} flex flex-col `}>
+                <div
+                  className={`${props.makeVisible} bg-white rounded-2xl  mt-5 place-self-center p-4 text-black z-10 hover:bg-white hover:text-yellow-600 `}
+                  onClick={() => {
+                    openGetStartedPageScreen();
+                  }}
+                >
+                  <p>Register</p>
+                </div>
+              </div>
+            </div>
 
-           <div className={` text-white h-[54px]  mt-[10px] size-[50px] w-[300px] ms-[20px] text-3xl font-bold   leading-[50px] text-center place-self-center ${props.lineHeight} ${props.textSize}`}>
-           {props.text}
-           </div>
-
-           <div className="mt-[140px] text-white font-bold text-center z-10 text-balance text-xl">
-           Carefully selected, best homes thats meet with your rental needs   
-           </div>
-
-           <div className="bg-yellow-600 rounded-2xl  mt-12 place-self-center p-4 text-white z-10 hover:bg-white hover:text-yellow-600" onClick={()=>{
-           
-              if(props.buttonOneText === "Get Started"){
-                 openGetStartedPageScreen()
-             
-              }else{
-                
-                navigate('/login')
-              }
-           }}>
-           <p>{props.buttonOneText}</p>
-           </div>
-
-           <div className={`${props.makeVisible} bg-white rounded-2xl  mt-5 place-self-center p-4 text-black z-10 hover:bg-white hover:text-yellow-600 `}  onClick={()=>{
-
-openGetStartedPageScreen()
-}}>
-            <p>Register</p>
+            <div className="sideImage-Container hidden xl:block m-3 h-[200px] ">
+              <img className="h-[400px]" src={sideImage} />
+            </div>
           </div>
-
         </div>
-
-     </div>
-
-     <div className="sideImage-Container hidden xl:block m-3 h-[200px] ">
-
-       <img className="h-[400px]" src={sideImage}  />
-     
-     </div>
-
-        </div>
-
+      </div>
     </div>
+  );
+};
 
-       </div> 
-    
-       </div>
-     );
-
-
-
-     
-}
-
-export default NavBarComponent
+export default NavBarComponent;
