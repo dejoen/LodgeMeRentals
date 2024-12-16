6fc65962a4028ee6e02ebdf4d97458c26ce3867b
Devjoe <benagu477@gmail.com>
2024-12-16 12:53:50 +0100

    update now on profile

--- src/features/login/screen/LoginScreen.jsx
+++ src/features/login/screen/LoginScreen.jsx
@@ -43,7 +43,7 @@ import { io } from 'socket.io-client'
                        agentReducerDispatcher({TYPE:"Authentication",payload:{
                             ...agentReducerState,
                             isLoggedIn:true,
-                            data:user
+                            data:connectedUser
                          }})
                         
                     navigate('/agent/dashboard')
@@ -54,7 +54,7 @@ import { io } from 'socket.io-client'
                         clientReducerDispatcher({TYPE:"Authentication",payload:{
                             ...clientReducerState,
                             isLoggedIn:true,
-                            data:user
+                            data:connectedUser
                          }})
                        
                       
