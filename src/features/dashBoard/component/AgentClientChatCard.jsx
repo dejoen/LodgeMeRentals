ff8d790b46d8fa78c2feabcb4071e03505ca6990
Devjoe <benagu477@gmail.com>
2025-02-19 19:46:52 +0100

    update on code

--- src/features/dashBoard/component/AgentClientChatCard.jsx
+++ src/features/dashBoard/component/AgentClientChatCard.jsx
@@ -2,7 +2,7 @@
  
  const AgentClientChatCard = ({data}) => {
     return  <div className="w-fit h-fit">
-        <img  className='w-[60px] h-[60px] bg-orange-600 rounded-full' src={data.senderId.userProfile.profileImage ?? '/'} />
+        <img  className='w-[60px] h-[60px] bg-orange-600 rounded-full' src={data && data.senderId ?data.senderId.userProfile.profileImage : '/'} />
     </div>
  }
 
